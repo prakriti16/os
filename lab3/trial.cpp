85d492d4566cae10eb8e13c55294e277e8359a89
prakriti16 <70594062+prakriti16@users.noreply.github.com>
2024-09-10 16:19:11 +0530

    trial code

---
+++ lab3/trial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <unistd.h>
+#include <signal.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	if(argc != 3)
+	{
+		cout <<"usage: ./partitioner.out  <scheduling-algorithm> <path-to-workload-description-file> \nprovided arguments:\n";
+		for(int i = 0; i < argc; i++)
+			cout << argv[i] << "\n";
+		return -1;
+	}
+    
+}
