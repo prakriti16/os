8a1abc1fa3216f7d9449c8ced955a4b1ce924d1b
vkay0725 <153687089+vkay0725@users.noreply.github.com>
2024-10-12 23:04:28 +0530

    Update p2q2.cpp

--- lab4/src/p2q2.cpp
+++ lab4/src/p2q2.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
+#include <bits/stdc++.h> 
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include <sys/sem.h>
 #include <unistd.h>
-#include <wait.h>
+#include <sys/wait.h>
 #include <cstring>
 #include <cstdint>
 #include <chrono>
-#include "libppm.h"  //includes definition of `image_t`
+#include "libppm.h"  // Includes definition of `image_t`
+
 using namespace std;
 using namespace std::chrono;
 
@@ -25,43 +26,43 @@ struct image_t* S2_find_details(struct image_t *input_image, struct image_t *smo
 struct image_t* S3_sharpen(struct image_t *input_image, struct image_t *details_image);
 
 void setup_ipc() {
-    shmid1 = shmget(IPC_PRIVATE, sizeof(struct image_t), IPC_CREAT | 0666); //s1 to s2 shared mem
+    shmid1 = shmget(IPC_PRIVATE, sizeof(struct image_t), IPC_CREAT | 0666);
     if (shmid1 < 0) {
         perror("shmget");
         exit(1);
     }
 
-    shmid2 = shmget(IPC_PRIVATE, sizeof(struct image_t), IPC_CREAT | 0666); //s2 to s3 shared mem
+    shmid2 = shmget(IPC_PRIVATE, sizeof(struct image_t), IPC_CREAT | 0666);
     if (shmid2 < 0) {
         perror("shmget");
         exit(1);
     }
 
-    semid = semget(IPC_PRIVATE, 2, IPC_CREAT | 0666); // sem sets ; sem_num 0 & sem_num 1
+    semid = semget(IPC_PRIVATE, 2, IPC_CREAT | 0666);
     if (semid < 0) {
         perror("semget");
         exit(1);
     }
 
-    union semun sem_union; //initial to 0, locked
+    union semun sem_union;
     sem_union.val = 0;
     for (int i = 0; i < 2; ++i) {
         semctl(semid, i, SETVAL, sem_union);
     }
 }
 
-void sem_signal(int sem_num) { //increm sem val,resource avail
+void sem_signal(int sem_num) {
     struct sembuf op;
     op.sem_num = sem_num;
-    op.sem_op = 1;  //unlock
+    op.sem_op = 1;  // Unlock
     op.sem_flg = 0;
     semop(semid, &op, 1);
 }
 
-void sem_wait(int sem_num) { //decremen sem val
+void sem_wait(int sem_num) {
     struct sembuf op;
     op.sem_num = sem_num;
-    op.sem_op = -1;  //lock
+    op.sem_op = -1;  // Lock
     op.sem_flg = 0;
     semop(semid, &op, 1);
 }
@@ -70,12 +71,16 @@ void run_parallel(struct image_t* input_image, const char* output_filename) {
     setup_ipc();
 
     pid_t pid_s1 = fork();
-    if (pid_s1 == 0) { //s1 process
-        struct image_t *shm_s1 = (struct image_t*)shmat(shmid1, NULL, 0); //attaches shared mem shm1 to curr process'(s1) address space
+    if (pid_s1 == 0) { // S1 process
+        struct image_t *shm_s1 = (struct image_t*)shmat(shmid1, NULL, 0);
+        if (shm_s1 == (struct image_t*)(-1)) {
+            perror("shmat");
+            exit(1);
+        }
         for (int i = 0; i < 1000; ++i) { 
-            struct image_t* smooth_img = S1_smoothen(input_image); 
+            struct image_t* smooth_img = S1_smoothen(input_image);
             memcpy(shm_s1, smooth_img, sizeof(struct image_t)); // Copy result to shared memory
-            sem_signal(0);  //signals s2 , semaphore assoc with sem_num 0 is ++
+            sem_signal(0);  // Signal S2
             delete smooth_img; // Free the temporary smoothened image
         }
         shmdt(shm_s1);
@@ -83,31 +88,49 @@ void run_parallel(struct image_t* input_image, const char* output_filename) {
     }
 
     pid_t pid_s2 = fork();
-    if (pid_s2 == 0) { //process s2
-        struct image_t *shm_s1 = (struct image_t*)shmat(shmid1, NULL, 0); //attaches shared mem sh1 (for s2 to access it)
-        struct image_t *shm_s2 = (struct image_t*)shmat(shmid2, NULL, 0); // attaches shared mem sh2 to s2's add space(for s2 to write in it)
+    if (pid_s2 == 0) { // S2 process
+        struct image_t *shm_s1 = (struct image_t*)shmat(shmid1, NULL, 0);
+        struct image_t *shm_s2 = (struct image_t*)shmat(shmid2, NULL, 0);
+        if (shm_s1 == (struct image_t*)(-1) || shm_s2 == (struct image_t*)(-1)) {
+            perror("shmat");
+            exit(1);
+        }
         for (int i = 0; i < 1000; ++i) { 
-            sem_wait(0);  //waits for s1 to finish
+            sem_wait(0);  // Wait for S1 to finish
             struct image_t* details_img = S2_find_details(input_image, shm_s1);
             memcpy(shm_s2, details_img, sizeof(struct image_t)); // Copy result to shared memory
             sem_signal(1);  
             delete details_img; // Free the temporary details image
         }
-        shmdt(shm_s1); //detach shm1 & 2
+        shmdt(shm_s1);
         shmdt(shm_s2);
         exit(0);
     }
 
-    pid_t pid_s3 = fork(); //process 3
+    pid_t pid_s3 = fork(); // S3 process
     if (pid_s3 == 0) {
         struct image_t *shm_s2 = (struct image_t*)shmat(shmid2, NULL, 0);
+        if (shm_s2 == (struct image_t*)(-1)) {
+            perror("shmat");
+            exit(1);
+        }
+
         struct image_t *sharpened_image = nullptr;
         for (int i = 0; i < 1000; ++i) { 
-            sem_wait(1); // wait for s2 to finish
+            sem_wait(1); // Wait for S2 to finish
             sharpened_image = S3_sharpen(input_image, shm_s2);
+            delete sharpened_image; // Free the sharpened image
         }
-        write_ppm_file((char*)output_filename, sharpened_image); 
-        delete sharpened_image; // Free the sharpened image
+        
+        // Write the final image after all iterations
+        sharpened_image = S3_sharpen(input_image, shm_s2);
+        int result = write2_ppm_file((char*)output_filename, sharpened_image); // Call write2_ppm_file instead of write_ppm_file
+        if (result != 0) {
+            perror("Error writing output file");
+        }
+        delete sharpened_image;  // Free the final sharpened image
+        
+        shmdt(shm_s2);
         exit(0);
     }
 
@@ -133,7 +156,7 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-// S1, S2, S3 function definitions from your previous code
+// Function definitions
 struct image_t* S1_smoothen(struct image_t *input_image) {
     int width = input_image->width;
     int height = input_image->height;
@@ -185,47 +208,42 @@ struct image_t* S2_find_details(struct image_t *input_image, struct image_t *smo
     int width = input_image->width;
     int height = input_image->height;
 
-    struct image_t* detail_img = new image_t;
-    detail_img->width = width;
-    detail_img->height = height;
+    struct image_t* details_img = new image_t;
+    details_img->width = width;
+    details_img->height = height;
 
-    detail_img->image_pixels = new uint8_t**[height];
+    details_img->image_pixels = new uint8_t**[height];
     for (int i = 0; i < height; i++) {
-        detail_img->image_pixels[i] = new uint8_t*[width];
+        details_img->image_pixels[i] = new uint8_t*[width];
         for (int j = 0; j < width; j++) {
-            detail_img->image_pixels[i][j] = new uint8_t[3];
+            details_img->image_pixels[i][j] = new uint8_t[3];
 
             for (int k = 0; k < 3; k++) {
-                detail_img->image_pixels[i][j][k] = abs(input_image->image_pixels[i][j][k] - smoothened_image->image_pixels[i][j][k]);
+                details_img->image_pixels[i][j][k] = abs(input_image->image_pixels[i][j][k] - smoothened_image->image_pixels[i][j][k]);
             }
         }
     }
-
-    return detail_img;
+    return details_img;
 }
 
 struct image_t* S3_sharpen(struct image_t *input_image, struct image_t *details_image) {
     int width = input_image->width;
     int height = input_image->height;
 
-    struct image_t* sharpen_img = new image_t;
-    sharpen_img->width = width;
-    sharpen_img->height = height;
+    struct image_t* sharpened_image = new image_t;
+    sharpened_image->width = width;
+    sharpened_image->height = height;
 
-    sharpen_img->image_pixels = new uint8_t**[height];
+    sharpened_image->image_pixels = new uint8_t**[height];
     for (int i = 0; i < height; i++) {
-        sharpen_img->image_pixels[i] = new uint8_t*[width];
+        sharpened_image->image_pixels[i] = new uint8_t*[width];
         for (int j = 0; j < width; j++) {
-            sharpen_img->image_pixels[i][j] = new uint8_t[3];
+            sharpened_image->image_pixels[i][j] = new uint8_t[3];
 
             for (int k = 0; k < 3; k++) {
-                sharpen_img->image_pixels[i][j][k] = input_image->image_pixels[i][j][k] + details_image->image_pixels[i][j][k];
-                if (sharpen_img->image_pixels[i][j][k] > 255) {
-                    sharpen_img->image_pixels[i][j][k] = 255;
-                }
+                sharpened_image->image_pixels[i][j][k] = std::min(255, std::max(0, (int)input_image->image_pixels[i][j][k] + (int)details_image->image_pixels[i][j][k]));
             }
         }
     }
-
-    return sharpen_img;
+    return sharpened_image;
 }
