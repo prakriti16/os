807d88c204a8ded38a916f98b8905f793e4cc3af
prakriti16 <70594062+prakriti16@users.noreply.github.com>
2024-10-13 16:05:57 +0530

    Update im2_3.cpp

--- lab4/src/im2_3.cpp
+++ lab4/src/im2_3.cpp
@@ -12,9 +12,18 @@ using namespace std;
 // Declare atomic flags for synchronization
 std::atomic<bool> s1_done(false);
 std::atomic<bool> s2_done(false);
+std::atomic_flag lock_s1 = ATOMIC_FLAG_INIT;
+std::atomic_flag lock_s2 = ATOMIC_FLAG_INIT;
+void test_and_set_lock(std::atomic_flag& lock) {
+    // Spin-lock until the flag is successfully set
+    while (lock.test_and_set(std::memory_order_acquire)) {
+        // Busy-wait until the lock is acquired
+    }
+}
+void release_lock(std::atomic_flag& lock) {
+    lock.clear(std::memory_order_release); // Release the lock
+}
 
-pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
-pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
 
 struct thread_data {
     struct image_t* input_image;
@@ -133,7 +142,7 @@ struct image_t* S2_find_details(struct image_t *input_image, struct image_t *smo
 struct image_t* S3_sharpen(struct image_t *input_image, struct image_t *details_image)
 {
 	// TODO
-		   int width = input_image->width;
+	int width = input_image->width;
     int height = input_image->height;
 
     struct image_t* sharpen_img = new image_t;
@@ -162,14 +171,12 @@ struct image_t* S3_sharpen(struct image_t *input_image, struct image_t *details_
 
 
 void* S1_smoothen_thread(void* arg) {
-    // cout<<"s1"<<endl;
-
     thread_data* data = (thread_data*)arg;
 
-    pthread_mutex_lock(&mutex1); // Locking to synchronize S1
+    test_and_set_lock(lock_s1); // Replace mutex lock with test-and-set lock
     data->smoothened_image = S1_smoothen(data->input_image);
-    // cout<<"s1_smooth"<<generatehash(data->smoothened_image)<<endl;
-    pthread_mutex_unlock(&mutex1); // Unlocking after smoothen is done
+    //cout << "s1_smooth" << generatehash(data->smoothened_image) << endl;
+    release_lock(lock_s1); // Replace mutex unlock with test-and-set unlock
 
     s1_done.store(true); // Indicate that S1 is done
     pthread_exit(nullptr);
@@ -177,40 +184,35 @@ void* S1_smoothen_thread(void* arg) {
 
 
 void* S2_find_details_thread(void* arg) {
-
     thread_data* data = (thread_data*)arg;
 
-    // Wait until S1 is done
     while (!s1_done.load()) {
         usleep(100); // Sleep for a while to avoid busy-waiting
     }
 
-    pthread_mutex_lock(&mutex2); // Locking for S2 synchronization
-    // cout<<"s2_smooth"<<generatehash(data->smoothened_image)<<endl;
-
+    test_and_set_lock(lock_s2); // Replace mutex lock with test-and-set lock
+    //cout << "s2_smooth" << generatehash(data->smoothened_image) << endl;
     data->details_image = S2_find_details(data->input_image, data->smoothened_image);
-    // cout<<"s2_det"<<generatehash(data->details_image)<<endl;
-    pthread_mutex_unlock(&mutex2);
+    //cout << "s2_det" << generatehash(data->details_image) << endl;
+    release_lock(lock_s2); // Replace mutex unlock with test-and-set unlock
 
     s2_done.store(true); // Indicate that S2 is done
     pthread_exit(nullptr);
 }
 
-// Sharpening function (S3)
+
 void* S3_sharpen_thread(void* arg) {
-    // cout<<"s3"<<endl;
     thread_data* data = (thread_data*)arg;
 
-    // Wait until S2 is done
     while (!s2_done.load()) {
         usleep(100); // Sleep for a while to avoid busy-waiting
     }
-    // cout<<"s3_det"<<generatehash(data->details_image)<<endl;
-    // Perform sharpening
+    //cout<<"s3_det"<<generatehash(data->details_image)<<endl;
     data->sharpened_image = S3_sharpen(data->input_image, data->details_image);
-    // cout<<"s3_sharp"<<generatehash(data->sharpened_image)<<endl;
+    //cout<<"s3_sharp"<<generatehash(data->sharpened_image)<<endl;
     pthread_exit(nullptr);
 }
+
 
 
 int main(int argc, char **argv) {
@@ -218,11 +220,13 @@ int main(int argc, char **argv) {
         std::cerr << "usage: ./a.out <input_image_path> <output_image_path>" << std::endl;
         return 1;
     }
+    //freopen("input.txt","w",stdout);
     
-    // Start measuring time using chrono
     auto start = high_resolution_clock::now();
         struct image_t* input_image = read_ppm_file(argv[1]);
-
+    auto s1 = high_resolution_clock::now();
+    auto s2 = high_resolution_clock::now();
+    auto s3 = high_resolution_clock::now();
     for (int i = 0; i < 1000; ++i) {
         thread_data data;
         data.input_image = input_image;
@@ -234,15 +238,38 @@ int main(int argc, char **argv) {
 
         // Create threads
         pthread_t thread1, thread2, thread3;
-
+        if(i==0){
+            s1 = high_resolution_clock::now();
+        }
         // Create thread for S1 (smoothing)
         pthread_create(&thread1, nullptr, S1_smoothen_thread, &data);
-
+        if(i==999){
+            auto e1 = high_resolution_clock::now();
+            double t1 = duration_cast<duration<double>>(e1-s1).count();
+            cout << "s1: " << t1 << " seconds" << endl;
+        }
+        
+        if(i==0){
+            s2 = high_resolution_clock::now();
+        }
         // Create thread for S2 (finding details)
         pthread_create(&thread2, nullptr, S2_find_details_thread, &data);
+        if(i==999){
+            auto e2 = high_resolution_clock::now();
+            double t2 = duration_cast<duration<double>>(e2-s2).count();
+            cout << "s2: " << t2 << " seconds" << endl;
+        }
 
+        if(i==0){
+            s3 = high_resolution_clock::now();
+        }
         // Create thread for S3 (sharpening)
         pthread_create(&thread3, nullptr, S3_sharpen_thread, &data);
+        if(i==999){
+            auto e3 = high_resolution_clock::now();
+            double t3 = duration_cast<duration<double>>(e3-s3).count();
+            cout << "s3: " << t3 << " seconds" << endl;
+        }
 
         // Wait for threads to complete
         pthread_join(thread1, nullptr);
@@ -255,13 +282,11 @@ int main(int argc, char **argv) {
         free_image(data.details_image);
         if(i==999)
         {
-    
-    write_ppm_file(data.output_file, data.sharpened_image);
-
+            write_ppm_file(data.output_file, data.sharpened_image);
         }
         free_image(data.sharpened_image);
     }
-        free_image(input_image);
+    free_image(input_image);
 
 
     auto end = high_resolution_clock::now();
