68184e22c187c17d30e2c388806937681ae7ecce
prakriti16 <70594062+prakriti16@users.noreply.github.com>
2024-10-22 17:47:51 +0530

    Update pagetable.cpp

--- lab5/pagetable.cpp
+++ lab5/pagetable.cpp
@@ -1,8 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 class PageTable {
- 
+    public:
+        unordered_map<int64_t, int> pageTable;
+        void addPage(int page, int frame){}
+        void removePage(int page){}
+        int getFrame(int page){ 
+            for (auto it = pageTable.begin(); it != pageTable.end(); it++){
+                if(it->second == page){
+                    return it->first;
+                }
+            }
+            return -1;
+        }
+        int findfreeframe(){}
+        void printPageTable(){}
+        void printFrameTable(){}//stores frames in fifo order
+        PageTable(){
+        }
+
+
 };
 int main(){
-  return 0;
+    
+    return 0;
 }
